a07d670c0f9f71ec01a2de5d524be2915dad2cd3
Kshiitij1579 <kshitij.bhatnagar1579@gmail.com>
2026-01-10 17:50:24 +0530

    included engine headers

---
+++ main_content/engine.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "MoveGen.h"
+#include "BitBoard.h"
+#include "ChessConstants.hpp"
+#include <string>
+#include <string_view>
+#include <cstddef>
+#include <algorithm>
+#include <chrono>
+
+using namespace std::literals::string_literals;
+
