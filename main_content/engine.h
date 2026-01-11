efaa39b9f409beb67b284df4b733aade281bff90
Kshiitij1579 <kshitij.bhatnagar1579@gmail.com>
2026-01-11 15:58:45 +0530

    included engine.h private

--- main_content/engine.h
+++ main_content/engine.h
@@ -11,3 +11,27 @@
 
 using namespace std::literals::string_literals;
 
+
+class Engine
+{
+private:
+	MoveGen m_moveGen;
+
+	State m_state;
+	Move m_bestMove;
+	Move m_bestMoveFinal;
+
+	std::uint32_t m_depth;
+
+	bool m_stopSearch;
+	int m_timeCheckCount;
+	std::chrono::steady_clock::time_point m_searchStartTime;
+
+	std::uint32_t m_depthSearched;
+	std::uint32_t m_evaluations;
+	std::uint32_t m_nodes;
+	std::uint32_t m_prunes;
+	std::uint32_t m_mates;
+	std::size_t m_moveSource;
+	std::chrono::duration<double> m_seconds;
+}
