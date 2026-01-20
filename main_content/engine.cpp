593396b391db48dc27e1557d0fb0eb178d8f815e
Kshiitij1579 <kshitij.bhatnagar1579@gmail.com>
2026-01-20 21:07:17 +0530

    minimax function

--- main_content/engine.cpp
+++ main_content/engine.cpp
@@ -76,3 +76,173 @@ std::size_t Engine::squareToIndex(std::string_view square)
 
 	return rank * RANK_MAX + file; //Final-Rank
 }
+
+int Engine::minimax(const State& state, const std::uint32_t depth, int alpha, int beta)
+{
+	m_nodes++;
+
+	if (depth == 0)
+	{
+		return evaluate(state);
+	}
+
+	//time cutoff for iterative deepening
+	if (m_stopSearch)
+	{
+		return state.whiteToMove() ? INT_MAX : INT_MIN;
+	}
+
+	//only test time every 1000 nodes to avoid frequent system calls
+	if (m_timeCheckCount >= TIME_EVALUATION_NODE_DELAY)
+	{
+		const auto now{ std::chrono::high_resolution_clock::now() };
+		const std::chrono::duration<float> duration{ now - m_searchStartTime };
+
+		if (static_cast<size_t>(duration.count()) >= MAX_EVALUATION_TIME_SECONDS)
+		{
+			m_stopSearch = true;
+			return state.whiteToMove() ? INT_MAX : INT_MIN;
+		}
+
+		m_timeCheckCount = 0;
+	}
+
+	m_timeCheckCount++;
+
+	if (state.whiteToMove())
+	{
+		MoveList moves;
+		m_moveGen.generateMoves(state, moves);
+		moves.sortMoveList();
+
+		int max_eval{ INT_MIN };
+		bool anyLegalMoves{ false };
+
+		for (Move move : moves.moves())
+		{
+			State new_state{ state };
+
+			if (makeMove(move, new_state))
+			{
+				anyLegalMoves = true;
+
+				new_state.flipSide();
+
+				const int eval = minimax(new_state, depth - 1, alpha, beta);
+
+				//time cutoff for iterative deepening
+				if (m_stopSearch)
+				{
+					return state.whiteToMove() ? INT_MAX : INT_MIN;
+				}
+
+				if (eval > max_eval)
+				{
+					max_eval = eval;
+
+					if (depth == m_depth)
+					{
+						m_bestMove = move;
+					}
+				}
+
+				if (alpha < eval)
+				{
+					alpha = eval;
+				}
+
+				if (beta <= alpha)
+				{
+					m_prunes++;
+					break;
+				}
+			}
+		}
+
+		if (anyLegalMoves)
+		{
+			return max_eval;
+		}
+		else
+		{
+			if (kingInCheck(state))
+			{
+				//white checkmate
+				m_mates++;
+				return INT_MIN + depth + 1;
+			}
+			else
+			{
+				//white stalemate
+				return 0;
+			}
+		}
+	}
+	else
+	{
+		MoveList moves;
+		m_moveGen.generateMoves(state, moves);
+		moves.sortMoveList();
+
+		int min_eval{ INT_MAX };
+		bool anyLegalMoves{ false };
+
+		for (Move move : moves.moves())
+		{
+			State new_state{ state };
+
+			if (makeMove(move, new_state))
+			{
+				anyLegalMoves = true;
+
+				new_state.flipSide();
+				const int eval = minimax(new_state, depth - 1, alpha, beta);
+
+				//time cutoff for iterative deepening
+				if (m_stopSearch)
+				{
+					return state.whiteToMove() ? INT_MAX : INT_MIN;
+				}
+
+				if (eval < min_eval)
+				{
+					min_eval = eval;
+
+					if (depth == m_depth)
+					{
+						m_bestMove = move;
+					}
+				}
+
+				if (beta > eval)
+				{
+					beta = eval;
+				}
+
+				if (beta <= alpha)
+				{
+					m_prunes++;
+					break;
+				}
+			}
+		}
+		if (anyLegalMoves)
+		{
+			return min_eval;
+		}
+		else
+		{
+			if (kingInCheck(state))
+			{
+				//black checkmate
+				m_mates++;
+				return INT_MAX - depth - 1;
+			}
+			else
+			{
+				//black stalemate
+				return 0;
+			}
+		}
+	}
+}
